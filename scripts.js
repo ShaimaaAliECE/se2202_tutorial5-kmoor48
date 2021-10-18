e34ceff1a30f7e7d2fc9d3af5ac412db7be38849
Kaitlyn Moore <you@example.com>
2021-10-18 12:51:19 -0400

    YEEHAW

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
 
+var userInitialization = document.createElement('button');
+userInitialization.innerText= "Start Game";
+button.textContent = mytext;
+document.getElementsById("textarea").innerText=mytext;
+var button = document.createElement("button");
+document.getElementById("buttonarea").appendChild(button);
+document.getElementById('game-over-lbl').appendChild(button);
+
+
 //initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
+let player1= 'X';
+let player2= 'O';
+document.nextPlayer('O');
 
-
+var table = document.getElementbyId('td');
+var button = document.createElement('button')
 //This call will create the buttons needed for the gameboard.
 createGameBoard()
 
 function createGameBoard()
-{
+{ 
+    document.getElementByTagName('td');
+    for(let i=0; i<10; i++ ){
+        table[i].innerHTML="<button>[ ]</button>";
+    }
+
+
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
    
 }
@@ -23,7 +42,8 @@ for (let i=0; i<btns.length; i++)
 
 // This function will be used to respond to a click event on any of the board buttons.
 function takeCell(event)
-{
+{ 
+
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
@@ -33,6 +53,8 @@ function takeCell(event)
     // Check if the game is over
     if (isGameOver())
     {
+         document.getElementbyId('game-over-lbl').innerText="Game Over";
+    
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
     }
 
@@ -40,7 +62,7 @@ function takeCell(event)
 }
 
 function isGameOver()
-{
+{ 
     // This function returns true if all the buttons are disabled and false otherwise 
    
 }
